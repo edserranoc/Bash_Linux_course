23b7b784f73932eef9a7fd9e0b3c0ef9777b2bbe
Edison Serrano <edserranoc@unal.edu.co>
2025-03-19 03:33:22 +0000

    End of week3

---
+++ Activity_03_bash_scripts/bash_files/bash-script-basics_2.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash -xv
+
+# This is where comments:go I
+# It can be useful to explain:the  purpose: of your code
+## Note you can also start your script with #! /usr/bin/bash:-xv for verbose debugging
+##https://tldp.org/LDP/Bash-Beginners—Guide/html/sect_02_03.html
+
+# Set strict mode. Causes shell to exit when a command fails
+set -e
+
+# Enables printing of shell input lines as they are read
+#set -v
+
+#-Enables print of command traces before executing command
+set -x
+
+# Set-a-variable
+
+variable="one"
+
+echo "This is a script with a variable: $variable"
